52c9a3c8dda674ece9f95ed549541e0298adad20
KevinLeNozer <kevin.lenozer@gmail.com>
2022-06-15 15:57:58 +0200

    Mission Projet Intervenant

---
+++ src/projet/Projet.java
@@ -0,0 +1,34 @@
+package projet;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Projet {
+    private String nom;
+    private LocalDate debut;
+    private LocalDate fin;
+    private double prixFactureMO;
+
+    private ArrayList<Mission> missions = new ArrayList<>();
+
+    public Projet(String nom, LocalDate debut, LocalDate fin, double prixFactureMO,
+                  ArrayList<Mission> mission) {
+        this.nom = nom;
+        this.debut = debut;
+        this.fin = fin;
+        this.prixFactureMO = prixFactureMO;
+        this.missions = mission;
+    }
+    
+    public double cumulCoutMO() {
+        double cout = 0;
+        for (Mission mission : missions) {
+            cout += mission.getExecutant().getTauxHoraire() * mission.getNbrHeureEffectues();
+        }
+        return cout;
+    }
+
+    public double margeBruteCourante() {
+        return  prixFactureMO - this.cumulCoutMO();
+    }
+}
